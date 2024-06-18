f64001eb8866d21bde9080ba9650bf611b15361d
Alberto Rojo Gomez <argr6@gcloud.ua.es>
2024-06-18 17:10:31 +0200

    dsksd

--- L02-Plantillas/zodiaco-L02/src/main/java/pa/Prueba.java
+++ L02-Plantillas/zodiaco-L02/src/main/java/pa/Prueba.java
@@ -7,8 +7,18 @@ public class Prueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    	
-    	System.out.println("\n\n Ejercicio pendiente de realizar \n\n");
+    	Zodiaco f1 = new Zodiaco(23,9);
+    	Zodiaco f2 = new Zodiaco(9,40);
+    	Zodiaco f3 = new Zodiaco(-3,8);
+    	Zodiaco f4 = new Zodiaco(23,11);
+    	Zodiaco f5 = new Zodiaco(21,1);
+    	Zodiaco f6 = new Zodiaco(23,8);
+    	System.out.println(f1.obtener_signo());
+    	System.out.println(f2.obtener_signo());
+    	System.out.println(f3.obtener_signo());
+    	System.out.println(f4.obtener_signo());
+    	System.out.println(f5.obtener_signo());
+    	System.out.println(f6.obtener_signo());
  
     }
 }    
--- L02-Plantillas/zodiaco-L02/src/main/java/pa/Zodiaco.java
+++ L02-Plantillas/zodiaco-L02/src/main/java/pa/Zodiaco.java
@@ -26,15 +26,266 @@ public class Zodiaco {
        falso en caso contrario   
     */  
     private boolean fecha_valida() {
-        
-       return false;
+       boolean valido = false;
+       switch(mes) {
+       	case 1:
+       	case 3:
+       	case 4:
+       	case 5:
+       	case 6:
+       	case 7:
+       	case 8:
+       	case 9:
+       	case 10:
+       	case 11:
+       	case 12:
+       		switch(dia) {
+   		 	case 1:
+   		 	case 2:
+   		 	case 3:
+   		 	case 4:
+   		 	case 5:
+   		 	case 6:
+   		 	case 7:
+   		 	case 8:
+   		 	case 9:
+   		 	case 10:
+   		 	case 11:
+   		 	case 12:
+   		 	case 13:
+   		 	case 14:
+   		 	case 15:
+   		 	case 16:
+   		 	case 17:
+   		 	case 18:
+   		 	case 19:
+   		 	case 20:
+   		 	case 21:
+   		 	case 22:
+   		 	case 23:
+   		 	case 24:
+   		 	case 25:
+   		 	case 26:
+   		 	case 27:
+   		 	case 28:
+   		 	case 29:
+   		 	case 30:
+   		 	case 31:
+   		 		valido = true;
+   		 		break;
+   		 	
+   		}
+       	case 2:
+       		switch(dia) {
+   		 	case 1:
+   		 	case 2:
+   		 	case 3:
+   		 	case 4:
+   		 	case 5:
+   		 	case 6:
+   		 	case 7:
+   		 	case 8:
+   		 	case 9:
+   		 	case 10:
+   		 	case 11:
+   		 	case 12:
+   		 	case 13:
+   		 	case 14:
+   		 	case 15:
+   		 	case 16:
+   		 	case 17:
+   		 	case 18:
+   		 	case 19:
+   		 	case 20:
+   		 	case 21:
+   		 	case 22:
+   		 	case 23:
+   		 	case 24:
+   		 	case 25:
+   		 	case 26:
+   		 	case 27:
+   		 	case 28:
+   		 		valido = true;
+   		 		break;	 	
+   		}
+       }
+       return valido;
     }
  
     /**
      * Devuelve el nombre del signo asociado al día y mes de nacimiento, o el mensaje "Fecha inválida"   
     */
     public String obtener_signo() {
+ 
+        if(fecha_valida()) {
+        	
+        	switch(mes) {
+        	case 1:
+        	case 2:
+           	case 3:
+           	case 4:
+           		switch(dia) {
+	       		 	case 1:
+	       		 	case 2:
+	       		 	case 3:
+	       		 	case 4:
+	       		 	case 5:
+	       		 	case 6:
+	       		 	case 7:
+	       		 	case 8:
+	       		 	case 9:
+	       		 	case 10:
+	       		 	case 11:
+	       		 	case 12:
+	       		 	case 13:
+	       		 	case 14:
+	       		 	case 15:
+	       		 	case 16:
+	       		 	case 17:
+	       		 	case 18:
+	       		 	case 19:
+	       		 	case 20:
+	       		 		signo = "Capricornio";
+	       		 		break;
+	       		 	case 21:
+	       		 	case 22:
+	       		 	case 23:
+	       		 	case 24:
+	       		 	case 25:
+	       		 	case 26:
+	       		 	case 27:
+	       		 	case 28:
+	       		 	case 29:
+	       		 	case 30:
+	       		 	case 31:
+		       		 	signo = "Acuario";
+	       		 		break;
+           		}
+           	case 5:
+           	case 6:
+           	case 7:
+           	case 8:
+           	case 9:
+           		switch(dia) {
+       		 	case 1:
+       		 	case 2:
+       		 	case 3:
+       		 	case 4:
+       		 	case 5:
+       		 	case 6:
+       		 	case 7:
+       		 	case 8:
+	       		 	switch(dia) {
+	       		 	case 1:
+	       		 	case 2:
+	       		 	case 3:
+	       		 	case 4:
+	       		 	case 5:
+	       		 	case 6:
+	       		 	case 7:
+	       		 	case 8:
+	       		 	case 9:
+	       		 	case 10:
+	       		 	case 11:
+	       		 	case 12:
+	       		 	case 13:
+	       		 	case 14:
+	       		 	case 15:
+	       		 	case 16:
+	       		 	case 17:
+	       		 	case 18:
+	       		 	case 19:
+	       		 	case 20:       		 		
+	       		 	case 21:
+	       		 	case 22:
+	       		 	case 23:
+	       		 	signo = "Leo";
+	   		 		break;
+	       		 	case 24:
+	       		 	case 25:
+	       		 	case 26:
+	       		 	case 27:
+	       		 	case 28:
+	       		 	case 29:
+	       		 	case 30:
+	       		 	case 31:
+		       		 	signo = "Virgo";
+	       		 		break;
+	       		}
+       		 	case 9:
+       		 	case 10:
+       		 	case 11:
+       		 	case 12:
+       		 	case 13:
+       		 	case 14:
+       		 	case 15:
+       		 	case 16:
+       		 	case 17:
+       		 	case 18:
+       		 	case 19:
+       		 	case 20:       		 		
+       		 	case 21:
+       		 	case 22:
+       		 	case 23:
+       		 	signo = "Virgo";
+   		 		break;
+       		 	case 24:
+       		 	case 25:
+       		 	case 26:
+       		 	case 27:
+       		 	case 28:
+       		 	case 29:
+       		 	case 30:
+       		 	case 31:
+	       		 	signo = "Libra";
+       		 		break;
+       		}
+           	case 10:
+           	case 11:
+           		switch(dia) {
+       		 	case 1:
+       		 	case 2:
+       		 	case 3:
+       		 	case 4:
+       		 	case 5:
+       		 	case 6:
+       		 	case 7:
+       		 	case 8:
+       		 	case 9:
+       		 	case 10:
+       		 	case 11:
+       		 	case 12:
+       		 	case 13:
+       		 	case 14:
+       		 	case 15:
+       		 	case 16:
+       		 	case 17:
+       		 	case 18:
+       		 	case 19:
+       		 	case 20:       		 		
+       		 	case 21:
+       		 	case 22:
+       		 	signo = "Escorpio";
+   		 		break;
+       		 	case 23:
+       		 	case 24:
+       		 	case 25:
+       		 	case 26:
+       		 	case 27:
+       		 	case 28:
+       		 	case 29:
+       		 	case 30:
+       		 	case 31:
+	       		 	signo = "Sagitario";
+       		 		break;
+       		}
+           	case 12:
+        	}
+        	
+        }else {
+        	signo = "Fecha inválida";
+        }
         
-        return "";
+        return signo;
     }    
 }
